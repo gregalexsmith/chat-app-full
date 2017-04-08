e52ed496dc9a593e1f531e0fc04e7ca56a60c55e
gregalexsmith <gregalexsmith@gmail.com>
2017-04-08 12:03:39 -0400

    gulp setup
    configuring gulp to run webpack builds

---
+++ gulpfile.babel.js
@@ -0,0 +1,55 @@
+import gulp from 'gulp';
+import webpack from 'webpack';
+import chalk from 'chalk';
+import rimraf from 'rimraf';
+import { create as createServerConfig } from './webpack.server';
+
+const $ = require("gulp-load-plugins")();
+
+//---------------------------------------
+// Public tasks
+
+// Clean build directories
+gulp.task("clean:server", cb => rimraf("./build", cb));
+gulp.task("clean:client", cb => rimraf("./public/build", cb));
+gulp.task("clean", gulp.parallel("clean:server", "clean:client"));
+
+gulp.task("dev:server", gulp.series("clean:server", devServerBuild));
+gulp.task("prod:server", gulp.series("clean:server", prodServerBuild));
+
+//---------------------------------------
+// Private Server Tasks
+const devServerWebpack = webpack(createServerConfig(true));
+const prodServerWebpack = webpack(createServerConfig(false));
+
+function devServerBuild(callback) {
+  // invoke the run function on the webpack bundler
+  devServerWebpack.run((error, stats) => {
+    outputWebpack("Dev:Server", error, stats);
+    // let gulp know the task is finished with the callback
+    callback();
+  });
+}
+
+function prodServerBuild(callback) {
+  prodServerWebpack.run((error, stats) => {
+    outputWebpack("Prod:Server", error, stats);
+    callback();
+  });
+}
+
+//---------------------------------------
+// Helpers
+function outputWebpack(label, error, stats) {
+  if (error) {
+    throw new Error(error);
+  }
+  // handle errors from babel, es-lint, etc..
+  if (stats.hasErrors()) {
+    $.util.log(stats.toString({colors: true}));
+  } else {
+    const time = stats.endTime = stats.startTime;
+    $.util.log(chalk.bgGreen(`Build ${label} in ${time} ms`));
+  }
+  $.util.log(stats.toString());
+}
