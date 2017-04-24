2c6817e9d5b6b58b534abeb3f4ccfd9f48fdef2b
gregalexsmith <gregalexsmith@gmail.com>
2017-04-24 18:12:17 -0400

    adding items - client side
    - accept playlist add item

--- src/client/components/playlist/list.js
+++ src/client/components/playlist/list.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import moment from 'moment';
+import {Observable} from "rxjs";
 
 import { ElementComponent } from '../../lib/component';
 import { PlaylistSortComponent } from './sort';
@@ -25,16 +26,50 @@ export class PlaylistListComponent extends ElementComponent {
 
     // ------------------
     // Playlist
-    this._playlist.state$
-      .filter(a => a.type == "list")
-      .compSubscribe(this, ({state}) => {
-        $list.empty();
-        itemsMap = {};
-        for (let source of state.list) {
-          const comp = new PlaylistItemComponent(source);
-          itemsMap[source.id] = comp;
-          comp.attach($list);
-        }
+    Observable.merge(
+      this._playlist.state$.first(),
+      this._playlist.actions$
+        .filter(a => a.type == "list")
+        .compSubscribe(this, ({state}) => {
+          $list.empty();
+          itemsMap = {};
+          for (let source of state.list) {
+            const comp = new PlaylistItemComponent(source);
+            itemsMap[source.id] = comp;
+            comp.attach($list);
+          }
+        })
+    );
+
+
+
+
+    this._playlist.actions$
+      .filter(a => a.type === "add")
+      .compSubscribe(this, ({source, addAfter}) => {
+        const comp = new PlaylistItemComponent(source);
+        comp.attach($list);
+
+        itemsMap[source.id] = comp;
+        this._addItem(comp, addAfter ? itemsMap[addAfter.id] : null);
+      });
+  }
+
+  _addItem(comp, addAfterComp) {
+    if (addAfterComp)
+      addAfterComp.$element.after(comp.$element);
+    else
+      this.$element.prepend(comp.$element);
+
+    // animage new item
+    const oldHeight = comp.$element.height();
+    comp.$element
+      .addClass("selected")
+      .css({ height: 0, opacity: 0 })
+      .animate({ height: oldHeight, opacity: 1 }, 250, () => {
+        comp.$element
+          .removeClass("selected")
+          .css({ height: "", opacity: "" });
       });
   }
 }
--- src/client/stores/playlist-store.js
+++ src/client/stores/playlist-store.js
@@ -4,20 +4,24 @@ import {validateAddSource} from "shared/validation/playlist";
 export class PlaylistStore {
 	constructor(server) {
 		const defaultState = { current: null, list: [], map: {} };
-
 		this._server = server;
 
 		// attach map handlers for actions recieved from the server
 		const events$ = Observable.merge(
-			server.on$("playlist:list").map(opList)
+			server.on$("playlist:list").map(opList),
+			server.on$("playlist:added").map(opAdd)
     );
 
 		// tie into our state, publish the operations
-		this.state$ = events$
+		this.actions$ = events$
 			.scan(({state}, op) => op(state), { state: defaultState })
-			.publishReplay(1);
+			.publish();
 
-		this.state$.connect();
+		this.state$ = this.actions$
+			.publishReplay(1)
+			.startWith({ state: defaultState });
+
+		this.actions$.connect();
 
 		server.on("connect", () => {
 			// request the playlist from the server on connection
@@ -50,5 +54,40 @@ function opList(sources) {
 			type: "list",
 			state: state
 		};
+	};
+}
+
+// update store with incoming playlist item
+function opAdd({source, afterId}) {
+	return state => {
+		let insertIndex = 0,
+			addAfter = null;
+
+		if (afterId !== -1) {
+			addAfter = state.map[afterId];
+			if (!addAfter)
+				return opError(state, `Could not add source ${source.title} after ${afterId}, as ${afterId} was not found`);
+
+			const afterIndex = state.list.indexOf(addAfter);
+			insertIndex = afterIndex + 1;
+		}
+
+		state.list.splice(insertIndex, 0, source);
+
+		return {
+			type: "add",
+			source: source,
+			addAfter: addAfter,
+			state: state
+		};
+	};
+}
+
+function opError(state, error) {
+	console.error(error);
+	return {
+		type: "error",
+		error: error,
+		state: state
 	};
 }
